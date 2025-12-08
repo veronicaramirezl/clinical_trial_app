4477d23a28143a0518e816d22f9ec73661ef7027
Veronica Ramirez Lopera <82114792+veronicaramirezl@users.noreply.github.com>
2025-12-08 11:43:46 -0500

    fix

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -66,6 +66,7 @@ def load_participant_data(participant_id):
 # -------------------------------------------------------
 # SAVE DEMOGRAPHICS  (Cols A to AC)
 # -------------------------------------------------------
+
 def save_demographics(user_data):
     sheet = get_sheet()
     if not sheet:
@@ -73,45 +74,49 @@ def save_demographics(user_data):
 
     try:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        p_id = user_data.get("participant_id")
+        p_id = user_data["participant_id"]
 
-        # EXACT FIXED COLUMN ORDER
+        # Force string values for Google Sheets
+        def clean(v):
+            return "" if v is None else str(v)
+
         row_values = [
-            timestamp,                                 # A
-            p_id,                                      # B
-            user_data.get("age", ""),                  # C
-            user_data.get("sex", ""),                  # D
-            user_data.get("gender_identity", ""),      # E
-            user_data.get("education", ""),            # F
-            user_data.get("insurance", ""),            # G
-            user_data.get("insurance_type", ""),       # H
-            user_data.get("english_proficiency", ""),  # I
-            user_data.get("income", ""),               # J
-            user_data.get("health_status", ""),        # K
-            user_data.get("chronic_conditions", ""),   # L
-            user_data.get("visit_frequency", ""),      # M
-            user_data.get("trust_web_1", ""),          # N
-            user_data.get("trust_web_2", ""),          # O
-            user_data.get("trust_web_3", ""),          # P
-            user_data.get("mistrust_1", ""),           # Q
-            user_data.get("mistrust_2", ""),           # R
-            user_data.get("mistrust_3", ""),           # S
-            user_data.get("mistrust_4", ""),           # T
-            user_data.get("discrim_1", ""),            # U
-            user_data.get("discrim_2", ""),            # V
-            user_data.get("discrim_3", ""),            # W
-            user_data.get("discrim_4", ""),            # X
-            user_data.get("discrim_healthcare", ""),   # Y
-            user_data.get("nutri_calories", ""),       # Z
-            user_data.get("nutri_carbs", ""),          # AA
-            user_data.get("nutri_protein", ""),        # AB
-            ""                                          # AC (video placeholder)
+            clean(timestamp),                 # A
+            clean(p_id),                      # B
+            clean(user_data.get("age")),      # C
+            clean(user_data.get("sex")),      # D
+            clean(user_data.get("gender_identity")),  # E
+            clean(user_data.get("education")),         # F
+            clean(user_data.get("insurance")),         # G
+            clean(user_data.get("insurance_type")),    # H
+            clean(user_data.get("english_proficiency")), # I
+            clean(user_data.get("income")),              # J
+            clean(user_data.get("health_status")),       # K
+            clean(user_data.get("chronic_conditions")),  # L
+            clean(user_data.get("visit_frequency")),     # M
+            clean(user_data.get("trust_web_1")),         # N
+            clean(user_data.get("trust_web_2")),         # O
+            clean(user_data.get("trust_web_3")),         # P
+            clean(user_data.get("mistrust_1")),          # Q
+            clean(user_data.get("mistrust_2")),          # R
+            clean(user_data.get("mistrust_3")),          # S
+            clean(user_data.get("mistrust_4")),          # T
+            clean(user_data.get("discrim_1")),           # U
+            clean(user_data.get("discrim_2")),           # V
+            clean(user_data.get("discrim_3")),           # W
+            clean(user_data.get("discrim_4")),           # X
+            clean(user_data.get("discrim_healthcare")),  # Y
+            clean(user_data.get("nutri_calories")),      # Z
+            clean(user_data.get("nutri_carbs")),         # AA
+            clean(user_data.get("nutri_protein")),       # AB
+            ""                                           # AC video placeholder
         ]
 
+        # Lookup row
         cell = sheet.find(p_id)
 
         if cell:
-            # Write exactly A:AC (29 columns)
+            # Update EXACT correct range (A to AC)
             sheet.update(f"A{cell.row}:AC{cell.row}", [row_values])
         else:
             sheet.append_row(row_values)
